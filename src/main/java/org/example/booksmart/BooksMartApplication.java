22046ecfae3026d7b138d7834cc93d7afe0d47c3
Mohamed Saber <mohamedsaber.developer@gmail.com>
2024-10-18 14:41:00 +0300

    first commit

---
+++ src/main/java/org/example/booksmart/BooksMartApplication.java
@@ -0,0 +1,13 @@
+package org.example.booksmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMartApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BooksMartApplication.class, args);
+    }
+    
+}
